67708e4fab03f2b06f9f0f3de7a9c2b12354f6ed
Emilian Utma <utma.emilian@gmail.com>
2021-05-08 19:37:14 +0300

    Log version at startup

--- src/main/java/ro/eu/infoagenda/ui/InfoAgendaMain.java
+++ src/main/java/ro/eu/infoagenda/ui/InfoAgendaMain.java
@@ -5,8 +5,16 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class InfoAgendaMain extends Application {
+    private static final Logger logger = LogManager.getLogger(InfoAgendaMain.class);
+    private static final String VERSION = "1.1";
+    static {
+        logger.info(String.format("Info agenda %s", VERSION));
+    }
+
 
     @Override
     public void start(Stage primaryStage) throws Exception {
